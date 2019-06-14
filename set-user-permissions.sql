79b157f561b3af24a2de3bbff812899138ae8665
Michael Astrauckas <mastrauckas@hotmail.com>
2019-06-14 14:20:49 -0400

    Changing permission sql files.

--- set-user-permissions.sql
+++ set-user-permissions.sql
@@ -1,277 +1,44 @@
--- To creae a user you must login
--- to the database you want to create
-DECLARE @login varchar(100) = NULL;
-DECLARE @user varchar(100) = @login + 'usr';
--- If you only want set privileges for 1 database.
-DECLARE @permissionsToDatabase VARCHAR(100) = NULL;
+-- To creae a user you must login 
+-- to the database you want to create 
 
-DECLARE @grant BIT = 0;
-DECLARE @revoke BIT = 0;
-DECLARE @deny BIT = 0;
+-- Create user
+--CREATE USER <user, normally put 'usr' after login name> FOR LOGIN <login name> WITH DEFAULT_SCHEMA=[dbo]
 
--- Grants the users permission to connect to a database.
-DECLARE @privilegeConnect BIT = 0;
-DECLARE @privileageShowPlan BIT = 0;
-DECLARE @privileageExecute BIT = 0;
-DECLARE @privileageSelect BIT = 0;
-DECLARE @privileageInsert BIT = 0;
-DECLARE @privileageUpdate BIT = 0;
-DECLARE @privileageDelete BIT = 0;
-DECLARE @privileageCreateTable BIT = 0;
-DECLARE @privileageCreateView BIT = 0;
-DECLARE @privileageCreateStoredProcedure BIT = 0;
-DECLARE @privileageCreateType BIT = 0;
-DECLARE @privileageCreateFunction BIT = 0;
-DECLARE @privileageCreateDefault BIT = 0;
-DECLARE @privileageAlterSchemaDbo BIT = 0;
---The REFERENCES permission on a table is needed to create a FOREIGN KEY constraint that references that table.
---The REFERENCES permission is needed on an object to create a FUNCTION or VIEW with the WITH SCHEMABINDING clause that references that object.
-DECLARE @privileageReference BIT = 0;
+-- Create a role
+-- CREATE ROLE <role name> AUTHORIZATION <role owner, can use CURRENT_USER, which is the default if AUTHORIZATION is left off>;
+-- ALTER ROLE <role name> ADD MEMBER <role, user, login>;    
 
-IF @grant =0 AND @revoke IS NULL AND @deny IS NULL
-BEGIN
-	SELECT '@privileage, @revoke and @deny can not be NULL but have to be 0';
-END
-IF @grant = 0 AND @revoke  = 0 AND @deny  = 0
-BEGIN
-	SELECT '@privileage, @revoke and @deny can not all be 0';
-END
-ELSE IF @grant = 1 AND (@revoke = 1 OR @deny = 1)
-BEGIN
-	SELECT 'You cannot have GRANT and REVOKE or DENY together';
-END
-ELSE IF @revoke  = 1 AND (@grant = 1 OR @deny = 1)
-BEGIN
-	SELECT 'You cannot have REVOKE and GRANT or DENY together';
-END
-ELSE IF @deny  = 1 AND (@grant = 1 OR @revoke = 1)
-BEGIN
-	SELECT 'You cannot have DENY and GRANT or REVOKE together';
-END
-ELSE IF @login IS NOT NULL AND @user IS NOT NULL
-BEGIN
-	IF NOT EXISTS (SELECT Name  
-				   FROM [sys].[database_principals]
-				   WHERE [type] = 'S' 
-				   AND Name = @user)
-	BEGIN
-		EXEC ('CREATE USER [' + @user + '] FOR LOGIN ['+@login+'] WITH DEFAULT_SCHEMA=[dbo]')
-	END
+-- What you want the user 
+--The REFERENCES permission on a table is needed to create a FOREIGN KEY constraint that references that table. 
+--The REFERENCES permission is needed on an object to create a FUNCTION or VIEW with the WITH SCHEMABINDING clause that references that object. 
+-- CONNECT, SHOWPLAN, EXECUTE, SELECT, INSERT, UPDATE, DELETE, REFERENCES
+-- CREATE TABLE, CREATE VIEW, CREATE PROCEDURE, CREATE TYPE, CREATE FUNCTION, CREATE DEFAULT
+-- ALTER ON SCHEMA::dbo, ALTER ON SCHEMA::<put schema>
+-- VIEW SERVER, STATE VIEW DATABASE STATE
+-- EXEC
+-- VIEW DEFINITION
+
+-- Can also use
+-- GRANT: Give permission
+-- DENY: Deny permission
+-- REMOVE: Removed granted permission
+
+-- GRANT <PRIVILEGES>  TO <role, user, login>;
+-- GRANT <PRIVILEGES> ON OBJECT::SCHEMA::<The Shema>, <object name like sp and tables> TO <role, user, login>;
+
+ -- Add user/login/role to another role
+-- Complete list on what each roles does: https://docs.microsoft.com/en-us/sql/relational-databases/security/authentication-access/database-level-roles
+-- public
+-- db_role_vs_admin
+-- db_owner
+-- db_accessadmin
+-- db_securityadmin
+-- db_ddladmin
+-- db_backupoperator
+-- db_datareader
+-- db_datawriter
+-- db_denydatareader
+-- db_denydatawriter
+ --exec sp_addrolemember '<role to be a member of>', '<roll to add as a memeber>'
 
 
-	DECLARE @permissions NVARCHAR(100);
-	IF @privilegeConnect = 1
-	BEGIN
-		SET @permissions = ' CONNECT ';
-	END
-
-	-- What you want the user
-	IF @privileageShowPlan = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', SHOWPLAN ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' SHOWPLAN ';
-		END
-	END
-
-	IF @privileageExecute = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', EXECUTE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' EXECUTE ';
-		END
-	END
-
-
-	IF @privileageSelect = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', SELECT ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' SELECT ';
-		END
-	END
-
-	IF @privileageInsert = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', INSERT ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' INSERT ';
-		END
-	END
-
-	IF @privileageUpdate = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', UPDATE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' UPDATE ';
-		END
-	END
-	  
-	IF @privileageDelete = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', DELETE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' DELETE ';
-		END
-	END
-
-	IF @privileageCreateTable = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE TABLE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE TABLE ';
-		END
-	END
-	 
-	IF @privileageCreateView = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE VIEW ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE VIEW ';
-		END
-	END
-	
-	IF @privileageCreateStoredProcedure = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE PROCEDURE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE PROCEDURE ';
-		END
-	END
-
-	IF @privileageCreateType = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE TYPE ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE TYPE ';
-		END
-	END
-	 
-	IF @privileageCreateFunction = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE FUNCTION ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE FUNCTION ';
-		END
-	END
-	 
-	IF @privileageCreateDefault = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', CREATE DEFAULT ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' CREATE DEFAULT ';
-		END
-	END
-
-	IF @privileageAlterSchemaDbo = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', ALTER ON SCHEMA::dbo ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' ALTER ON SCHEMA::dbo ';
-		END
-	END
-	 
-	IF @privileageReference = 1
-	BEGIN
-		IF LEN(@permissions) > 0
-		BEGIN
-			SET @permissions += ', REFERENCES ';
-		END
-		ELSE
-		BEGIN
-			SET @permissions = ' REFERENCES';
-		END
-	END
-
-	DECLARE @privilegeType NVARCHAR(100);
-
-	IF @grant = 1
-	BEGIN
-		SET @privilegeType = 'Grant';
-	END
-	ELSE IF @revoke = 1
-	BEGIN
-		SET @privilegeType = 'REVOKE';
-	END
-	ELSE IF @deny = 1
-	BEGIN
-		SET @privilegeType = 'DENY';
-	END
-
-	DECLARE @sqlToRun NVARCHAR(200);
-	IF LEN(@permissions) > 0
-	BEGIN
-		IF @permissionsToDatabase IS NULL
-		BEGIN
-			SET @sqlToRun = @privilegeType + @permissions + ' TO ' + @user + ';';
-		END
-		ELSE
-		BEGIN
-			SET @sqlToRun = @privilegeType + @permissions + ' ON ' + @permissionsToDatabase + ' TO ' + @user + ';'
-			
-		END
-
-		SELECT 'Executed ''' + @sqlToRun + '''.';
-		EXEC(@sqlToRun);
-	END
-	ELSE
-	BEGIN
-		SELECT 'No privileges to set.';
-	END
-END
-ELSE
-BEGIN
-	SELECT '@login or @user is NULL';
-END
--- view_user_permissions.sql
+++ view_user_permissions.sql
@@ -1,14 +1,5 @@
-DECLARE @login varchar(100) = NULL
-DECLARE @user varchar(100) = IIF(@login IS NULL, NULL, @login + 'usr')
+DECLARE @roleOrUser NVARCHAR(100) = NULL;
 
-
-SELECT p.name AS SqlUser, p.type AS Type, p.type_desc As TypeDescription, p.default_schema_name AS DefaultSchema, create_date As CreatedDate, modify_date AS ModifyDate,
-dp.class AS Class, permission_name As PermissionName, State, state_desc AS StateDescription
-FROM sys.database_principals p
-LEFT JOIN sys.database_permissions dp ON dp.grantee_principal_id = p.principal_id
-WHERE p.type = 'S'
-AND (name = @user OR @user  IS NULL)
-ORDER BY p.name, dp.permission_name
  -- Principal type:
 --A = Application role
 --C = User mapped to a certificate
@@ -19,3 +10,30 @@ ORDER BY p.name, dp.permission_name
 --S = SQL user
 --U = Windows user
 --X = External group from Azure Active Directory group or applications
+
+-- View permissions for role.
+SELECT up.name AS SqlUser, up.type AS Type, up.type_desc As TypeDescription, up.default_schema_name AS DefaultSchema, up.create_date As CreatedDate, up.modify_date AS ModifyDate,
+p.class AS Class, p.permission_name As PermissionName, p.State, p.state_desc AS StateDescription
+FROM sys.database_principals up
+LEFT JOIN sys.database_role_members rm  ON up.principal_id = rm.member_principal_id
+LEFT JOIN sys.database_permissions p ON p.grantee_principal_id = up.principal_id
+WHERE  (up.name = @roleOrUser OR @roleOrUser IS NULL)
+--AND p.type = 'R'
+ORDER BY up.name, p.permission_name
+
+-- What role are they part of
+SELECT up.name, rp.name
+FROM sys.database_role_members AS rm
+INNER JOIN sys.database_principals AS rp ON rm.role_principal_id = rp.principal_id
+INNER JOIN sys.database_principals AS up ON up.principal_id = rm.member_principal_id
+WHERE  (up.name = @roleOrUser OR @roleOrUser IS NULL)
+
+SELECT rp.name AS RoleName, ISNULL(up.name, 'No members') AS UserName   
+FROM sys.database_role_members AS rm  
+RIGHT OUTER JOIN sys.database_principals AS rp ON rm.role_principal_id = rp.principal_id  
+LEFT OUTER JOIN sys.database_principals AS up ON rm.member_principal_id = up.principal_id  
+WHERE  (rp.name = @roleOrUser OR @roleOrUser IS NULL)
+AND rp.type = 'R'
+ORDER BY rp.name;  
+
+
