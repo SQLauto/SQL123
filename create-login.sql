d2b4ad1a95ca8f381892058f437e1e84759127f8
Michael Astrauckas <mastrauckas@hotmail.com>
2019-06-21 16:06:39 -0400

    permissions.

--- create-login.sql
+++
@@ -1,6 +0,0 @@
-USE master
-
--- ALTER LOGIN <login> WITH DEFAULT_DATABASE = [new_default_database]
--- CREATE LOGIN <login name> WITH PASSWORD = <password>
--- CREATE LOGIN <login name> WITH PASSWORD = <password>, DEFAULT_DATABASE = <database-name>
--- DROP LOGIN <login name>
