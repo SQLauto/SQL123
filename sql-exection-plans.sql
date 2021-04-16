8e3e51cb274f466a32f63e31c32a7fca89a9ba68
Michael Astrauckas <michael.astrauckas@virtuescript.com>
2021-04-16 17:25:56 -0400

    Adding items to exection plan viewing.

--- sql-exection-plans.sql
+++ sql-exection-plans.sql
@@ -1,48 +1,80 @@
--- DBCC FREEPROCCACHE (0x060009006BCD5029505CD63DC502000001000000000000000000000000000000000000000000000000000000);
-DECLARE @queryLike NVARCHAR(MAX) = 'select Count(Logger)';
-DECLARE @databaseName NVARCHAR(MAX) = 'VirtueScriptImport';
+-- Removing Cache: https://www.sqlskills.com/blogs/glenn/eight-different-ways-to-clear-the-sql-server-plan-cache/
+-- DBCC FREEPROCCACHE; -- Remove all elements from the plan cache for the entire instance
+-- DBCC FREEPROCCACHE WITH NO_INFOMSGS; -- Flush the plan cache for the entire instance and suppress the regular completion message
+-- DBCC FREEPROCCACHE (0x06000800B9ED3F28B054757A0202000001000000000000000000000000000000000000000000000000000000);
+
+DECLARE @queryLike NVARCHAR(MAX) = NULL;
+DECLARE @databaseName NVARCHAR(MAX) = NULL;
 DECLARE @queryPlanLike NVARCHAR(MAX) = NULL;
 DECLARE @lastExecutedDateTime DATETIME2 = NULL;
 DECLARE @objectType NVARCHAR(MAX) = NULL;
+DECLARE @viewExectionPlans BIT = 1;
+DECLARE @viewHowManyOfObjectTypes BIT = 1;
+DECLARE @howManyRows INT = 10;
 
-SELECT TOP 10 
-len(sqlText.text) QueryLength, DB_NAME(qp.dbid) DatabaseName, 
-cp.cacheobjtype, cp.objtype,
-OBJECT_ID(qp.objectid) AS ObjectName, 
-qp.number, cp.plan_handle,
-qs.execution_count,cp.refcounts, CP.usecounts,
-qs.creation_time,qs.last_execution_time,
-qs.last_rows,qs.last_logical_reads, qs.last_logical_writes,qs.last_physical_reads,
-cp.size_in_bytes, cp.usecounts, sqlText.text
-,qp.query_plan
-FROM sys.dm_exec_cached_plans cp
-JOIN sys.dm_exec_query_stats qs ON cp.plan_handle = qs.plan_handle
-CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle)AS sqlText
-CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle)AS qp
-WHERE
-(
-    (@queryLike IS NOT NULL AND CAST(sqlText.text AS NVARCHAR(MAX)) LIKE '%' + @queryLike + '%')
-    OR @queryLike IS NULL
-)
-AND
-(
-    (@queryPlanLike IS NOT NULL AND CAST(qp.query_plan AS NVARCHAR(MAX)) LIKE '%' + @queryPlanLike + '%')
-    OR @queryPlanLike IS NULL
-)
-AND
-(
-    (@lastExecutedDateTime IS NOT NULL AND last_execution_time > @lastExecutedDateTime)
-    OR @lastExecutedDateTime IS NULL
-)
-AND
-(
-    (@objectType IS NOT NULL AND cp.objtype = @objectType)
-    OR @objectType IS NULL
-)
-AND
-(
-    (@databaseName IS NOT NULL AND DB_NAME(qp.dbid) = @databaseName)
-    OR @databaseName IS NULL
-)
-AND (text NOT LIKE '%@queryLike%')
-ORDER BY last_execution_time DESC
+IF ISNULL(@viewExectionPlans, 0) = 1 BEGIN
+
+    SELECT TOP(ISNULL(@howManyRows, 10))
+    qsp.plan_id AS QueryStorePlanId,
+    len(sqlText.text) QueryLength,
+    sqlText.text,
+    qs.creation_time,
+    qs.last_execution_time,
+    cp.cacheobjtype, 
+    cp.objtype,
+    DB_NAME(qp.dbid) DatabaseName, 
+    OBJECT_ID(qp.objectid) AS ObjectName, 
+    qs.last_rows,
+    qs.last_logical_reads, 
+    qs.last_logical_writes,
+    qs.last_physical_reads,
+    cp.size_in_bytes,
+    qs.execution_count AS NumberOfTimesPlanHasExecutedSinceLastCompiled,
+    cp.usecounts As NumberOfTimesCacheObjectLookedUp,
+    cp.refcounts AS NumberOfCacheObjectsReferencingThisCacheObject, 
+    qp.query_plan,
+    cp.plan_handle
+    FROM sys.dm_exec_cached_plans cp
+    JOIN sys.dm_exec_query_stats qs ON cp.plan_handle = qs.plan_handle
+    JOIN sys.query_store_plan qsp ON qs.query_plan_hash = qsp.query_plan_hash AND qs.plan_handle = QS.plan_handle
+    CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle)AS sqlText
+    CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle)AS qp
+    WHERE
+    (
+        (@queryLike IS NOT NULL AND CAST(sqlText.text AS NVARCHAR(MAX)) LIKE '%' + @queryLike + '%')
+        OR @queryLike IS NULL
+    )
+    AND
+    (
+        (@queryPlanLike IS NOT NULL AND CAST(qp.query_plan AS NVARCHAR(MAX)) LIKE '%' + @queryPlanLike + '%')
+        OR @queryPlanLike IS NULL
+    )
+    AND
+    (
+        (@lastExecutedDateTime IS NOT NULL AND qs.last_execution_time > @lastExecutedDateTime)
+        OR @lastExecutedDateTime IS NULL
+    )
+    AND
+    (
+        (@objectType IS NOT NULL AND cp.objtype = @objectType)
+        OR @objectType IS NULL
+    )
+    AND
+    (
+        (@databaseName IS NOT NULL AND DB_NAME(qp.dbid) = @databaseName)
+        OR @databaseName IS NULL
+    )
+    AND (text NOT LIKE '%@queryLike%')
+    ORDER BY last_execution_time DESC
+
+END
+IF ISNULL(@viewHowManyOfObjectTypes, 0) = 1 BEGIN
+
+SELECT objtype, 
+COUNT(*) as NumberOfPlans,
+SUM(CAST(size_in_bytes as bigint))/1024/1024 as SizeInMBs,
+AVG(usecounts) as AvgNumberOfTimesCacheObjectLookedUp
+FROM sys.dm_exec_cached_plans
+GROUP BY objtype
+
+END
