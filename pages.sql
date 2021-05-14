e4810be7c3cf79d788ddbde6f297be1b089d46e8
Michael Astrauckas <michael.astrauckas@virtuescript.com>
2021-05-14 07:20:24 -0400

    Added index id.

--- pages.sql
+++ pages.sql
@@ -4,18 +4,19 @@ DECLARE @tableName NVARCHAR(MAX) = NULL;
 -- View Pages for a table
 -- https://kwelsql.wordpress.com/2016/01/31/dbcc-ind-and-dbcc-page/
 -- <DatabaseName or DBID>', '<TableName or ObjectId>', <index number, or -1 to see all indexes page details>
--- DBCC IND('<database>', '<table>', -1)
+-- DBCC IND('<database>', '<table>', -1);
 
 -- View data for a page
 -- DatabaseName or DBID, PageFID, PageID [, printopt={0|1|2|3}])
- --DBCC TRACEON (3604)
- --DBCC PAGE('<database>', 1, 76713, 3)
- --DBCC TRACEOFF (3604)
+ --DBCC TRACEON (3604);
+ --DBCC PAGE('<database>', 1, 76713, 3);
+ --DBCC TRACEOFF (3604);
 
 SELECT
 OBJECT_SCHEMA_NAME(t.object_id) AS SchemaName,
 t.NAME AS TableName,
 i.name AS IndexName,
+i.index_id AS IndexId,
 FORMAT(p.rows, N'N0') AS RowCounts,
 FORMAT(SUM(a.total_pages), N'N0') AS TotalPages, 
 FORMAT(SUM(a.used_pages), N'N0') AS UsedPages,
@@ -30,5 +31,5 @@ AND t.is_ms_shipped = 0
 AND i.OBJECT_ID > 255
 AND (OBJECT_SCHEMA_NAME(t.object_id) = @schemaName OR @schemaName IS NULL)
 AND (OBJECT_NAME(t.object_id) = @tableName OR @tableName IS NULL)
-GROUP BY t.Name, p.Rows, i.name, t.object_id
+GROUP BY t.Name, p.Rows, i.name, t.object_id, i.index_id
 ORDER BY SUM(a.total_pages) DESC
